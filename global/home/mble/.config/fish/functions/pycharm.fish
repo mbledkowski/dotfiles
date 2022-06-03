2e56e0e3bc1c9c0f68a1b773bdcc83f17beddf92
mbledkowski <pub@mble.dk>
2022-06-03 15:09:05 +0200

    Major update

--- global/home/mble/.config/fish/functions/pycharm.fish
+++
@@ -1,3 +0,0 @@
-function pycharm --wraps=pycharm-professional --description 'alias pycharm pycharm-professional'
-  pycharm-professional $argv; 
-end
--- saturn/etc/nixos/packages/multimedia.nix
+++
@@ -1,8 +0,0 @@
-{pkgs, ...}:
-{
-  environment.systemPackages = with pkgs; [
-    spotifyd
-    spotify-qt
-    spotify-tui
-  ];
-}
