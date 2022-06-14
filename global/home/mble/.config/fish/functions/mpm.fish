3380358b5a2c5931f0e9e6357c43f6b7b9782585
Maciej Błędkowski <pub@mble.dk>
2022-06-14 03:43:57 +0200

    Renamed mpm to mpmdir in fish config

--- global/home/mble/.config/fish/functions/mpm.fish
+++
@@ -1,3 +0,0 @@
-function mpm --wraps='cd ~/Code/repos/mpmgg/' --description 'alias mpm cd ~/Code/repos/mpmgg/'
-  cd ~/Code/repos/mpmgg/ $argv; 
-end
---
+++ global/home/mble/.config/fish/functions/mpmdir.fish
@@ -0,0 +1,3 @@
+function mpmdir --wraps='cd ~/Code/repos/mpmgg/' --description 'alias mpmdir cd ~/Code/repos/mpmgg/'
+  cd ~/Code/repos/mpmgg/ $argv; 
+end
