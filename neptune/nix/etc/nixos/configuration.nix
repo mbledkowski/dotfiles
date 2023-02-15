d4abec748c35f92e0feb75df649ac66e0706e1a9
mbledkowski <pub@mble.dk>
2023-02-15 13:33:34 +0100

    Update configuration.nix with Nvidia drivers

--- neptune/nix/etc/nixos/configuration.nix
+++ neptune/nix/etc/nixos/configuration.nix
@@ -242,6 +242,16 @@
     };
   };
 
+  # Nvidia drivers
+  # NVIDIA drivers are unfree.
+  nixpkgs.config.allowUnfree = true;
+
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.opengl.enable = true;
+
+  # Optionally, you may need to select the appropriate driver version for your specific GPU.
+  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
+
   # System
   system = {
     # This value determines the NixOS release from which the default
