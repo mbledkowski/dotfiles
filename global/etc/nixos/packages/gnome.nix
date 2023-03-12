dd2e3ebfdeeefc1b8463b592b6f426f1fc7e72b9
mbledkowski <pub@mble.dk>
2023-03-13 00:00:02 +0100

    Regular update

--- global/etc/nixos/packages/gnome.nix
+++ global/etc/nixos/packages/gnome.nix
@@ -4,7 +4,8 @@
     gnome.gnome-tweaks
     gnome.gnome-boxes
     gnome.dconf-editor
-    gnomeExtensions.clipboard-indicator
+    # gnomeExtensions.clipboard-indicator
+    gnomeExtensions.pano
     gnomeExtensions.just-perfection
     gnomeExtensions.caffeine
     gnomeExtensions.night-theme-switcher
