376c9c49459a9414e58cd52cb0448ad66d505884
mbledkowski <pub@mble.dk>
2024-02-03 00:00:03 +0100

    Regular update

--- global/home/mble/.config/fish/conf.d/virtualfish-loader.fish
+++ global/home/mble/.config/fish/conf.d/virtualfish-loader.fish
@@ -1,6 +1,4 @@
-if test -f /etc/arch-release
-  set -g VIRTUALFISH_VERSION 2.5.5
-  set -g VIRTUALFISH_PYTHON_EXEC /home/mble/.pyenv/versions/3.7.16/bin/python
-  source /home/mble/.pyenv/versions/3.7.16/lib/python3.7/site-packages/virtualfish/virtual.fish
-  emit virtualfish_did_setup_plugins
-end
+set -g VIRTUALFISH_VERSION 2.5.5
+set -g VIRTUALFISH_PYTHON_EXEC /usr/bin/python
+source /home/mble/.local/lib/python3.12/site-packages/virtualfish/virtual.fish
+emit virtualfish_did_setup_plugins
