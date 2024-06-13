f5faf8a6b9f959a763c55542a0b203e95049963c
mbledkowski <pub@mble.dk>
2024-06-14 00:00:02 +0200

    Regular update

--- global/home/mble/.config/fish/config.fish
+++ global/home/mble/.config/fish/config.fish
@@ -20,8 +20,10 @@ if test -f /home/mble/anaconda3/bin/conda
 end
 # <<< conda initialize <<<
 
-
 # bun
 set --export BUN_INSTALL "/home/mble/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
 export PATH=/home/mble/.nimble/bin:/home/mble/.bun/bin:/home/mble/.pyenv/shims:/home/mble/.local/share/pnpm:/home/mble/anaconda3/condabin:/home/mble/.pyenv/bin:/home/mble/.local/bin:/home/mble/.yarn/bin:/home/mble/.tea/tea.xyz/v0.39.6/bin:/var/home/mble/.local/bin:/home/mble/.bun/bin:/home/mble/.local/share/pnpm:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin
+
+# Starship
+starship init fish | source
