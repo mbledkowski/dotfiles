81b6006932d4521108360603272d2739bab7f260
mbledkowski <pub@mble.dk>
2025-01-04 00:00:02 +0100

    Regular update

--- global/home/mble/.config/lvim/config.lua
+++ global/home/mble/.config/lvim/config.lua
@@ -84,66 +84,66 @@ lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enable = true
 
 -- DAP settings
-local home = os.getenv('HOME')
-local dap = require('dap')
-dap.adapters.node2 = {
-  type = 'executable',
-  command = 'node',
-  args = { home .. '/.local/share/nvim/lspinstall/node_modules/vscode-node-debug2/out/src/nodeDebug.js' },
-}
+-- local home = os.getenv('HOME')
+-- local dap = require('dap')
+-- dap.adapters.node2 = {
+--   type = 'executable',
+--   command = 'node',
+--   args = { home .. '/.local/share/nvim/lspinstall/node_modules/vscode-node-debug2/out/src/nodeDebug.js' },
+-- }
 
-dap.adapters.python = {
-  type = "executable",
-  command = "python3",
-  args = { "-m", "debugpy.adapter" },
-}
+-- dap.adapters.python = {
+--   type = "executable",
+--   command = "python3",
+--   args = { "-m", "debugpy.adapter" },
+-- }
 
-dap.configurations.javascript = {
-  {
-    name = 'Launch',
-    type = 'node2',
-    request = 'launch',
-    program = '${file}',
-    cwd = vim.loop.cwd(),
-    sourceMaps = true,
-    protocol = 'inspector',
-    console = 'integratedTerminal',
-  },
-  {
-    -- For this to work you need to make sure the node process
-    -- is started with the `--inspect` flag.
-    name = 'Attach to process',
-    type = 'node2',
-    request = 'attach',
-    processId = require('dap.utils').pick_process,
-  },
-}
+-- dap.configurations.javascript = {
+--   {
+--     name = 'Launch',
+--     type = 'node2',
+--     request = 'launch',
+--     program = '${file}',
+--     cwd = vim.loop.cwd(),
+--     sourceMaps = true,
+--     protocol = 'inspector',
+--     console = 'integratedTerminal',
+--   },
+--   {
+--     -- For this to work you need to make sure the node process
+--     -- is started with the `--inspect` flag.
+--     name = 'Attach to process',
+--     type = 'node2',
+--     request = 'attach',
+--     processId = require('dap.utils').pick_process,
+--   },
+-- }
 
-dap.configurations.typescript = {
-  {
-    name = "ts-node (Node2 with ts-node)",
-    type = "node2",
-    request = "launch",
-    cwd = vim.loop.cwd(),
-    runtimeArgs = { "-r", "ts-node/register" },
-    runtimeExecutable = "node",
-    args = { "--inspect", "${file}" },
-    sourceMaps = true,
-    skipFiles = { "<node_internals>/**", "node_modules/**" },
-  },
-  {
-    name = "Jest (Node2 with ts-node)",
-    type = "node2",
-    request = "launch",
-    cwd = vim.loop.cwd(),
-    runtimeArgs = { "--inspect-brk", "${workspaceFolder}/node_modules/.bin/jest" },
-    runtimeExecutable = "node",
-    args = { "${file}", "--runInBand", "--coverage", "false" },
-    sourceMaps = true,
-    port = 9229,
-    skipFiles = { "<node_internals>/**", "node_modules/**" },
-  },
-}
+-- dap.configurations.typescript = {
+--   {
+--     name = "ts-node (Node2 with ts-node)",
+--     type = "node2",
+--     request = "launch",
+--     cwd = vim.loop.cwd(),
+--     runtimeArgs = { "-r", "ts-node/register" },
+--     runtimeExecutable = "node",
+--     args = { "--inspect", "${file}" },
+--     sourceMaps = true,
+--     skipFiles = { "<node_internals>/**", "node_modules/**" },
+--   },
+--   {
+--     name = "Jest (Node2 with ts-node)",
+--     type = "node2",
+--     request = "launch",
+--     cwd = vim.loop.cwd(),
+--     runtimeArgs = { "--inspect-brk", "${workspaceFolder}/node_modules/.bin/jest" },
+--     runtimeExecutable = "node",
+--     args = { "${file}", "--runInBand", "--coverage", "false" },
+--     sourceMaps = true,
+--     port = 9229,
+--     skipFiles = { "<node_internals>/**", "node_modules/**" },
+--   },
+-- }
 
 -- dap.configurations.python = {
 --   {
