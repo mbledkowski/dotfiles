62135eb8d36639555a48ef9c13da4740282357d2
mbledkowski <pub@mble.dk>
2025-12-17 00:00:02 +0100

    Regular update

--- global/home/mble/.config/fish/functions/code.fish
+++ global/home/mble/.config/fish/functions/code.fish
@@ -1,4 +1,5 @@
 function code --description 'alias code code --enable-features=UseOzonePlatform --ozone-platform=wayland'
- command code --enable-features=UseOzonePlatform --ozone-platform=wayland $argv
-        
+ set __GL_SYNC_TO_VBLANK 0
+ set vblank_mode 0
+ command code --enable-features=UseOzonePlatform --ozone-platform=wayland --disable-gpu-vsync $argv
 end
